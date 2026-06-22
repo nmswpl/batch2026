e2a62039481f7ba7a589717087b97ab3727e5a66
Sivakumar <csivakumar@nmsworks.co.in>
2026-06-22 16:41:12 +0530

    Week 2, Day 1 StringManipulation sums are solved.

--- Siva/src/main/java/in/co/nmsworks/training/week2/day1/StringManipulation.java
+++ Siva/src/main/java/in/co/nmsworks/training/week2/day1/StringManipulation.java
@@ -90,7 +90,6 @@ public class StringManipulation {
 
     private static void printEachSentencesInTheNewLine() {
         String text = "The early morning sun cast a golden hue over the quiet village, where the scent of blooming jasmine mingled with the crisp, cool air. As the first light of day filtered through the canopy of ancient oak trees, the streets began to stir with the soft chatter of locals preparing for the day ahead. Children darted around with laughter, their voices a cheerful contrast to the serene landscape. In the heart of the village, the old bakery's doors creaked open, releasing the warm, inviting aroma of freshly baked bread, promising a comforting start to the day for everyone within reach.";
-        String[] textArray = text.split(".");
         for (String s : textArray) {
             System.out.println(s);
         }
