d95f796a153153eef9294b24eac4a462e90507ce
Saravana Pandian <saravanapandian@nmsworks.co.in>
2026-06-25 15:25:00 +0530

    Shaperunner completed

---
+++ pandii/src/main/java/in/co/nmsworks/training/week2/day3/ShapeRunner.java
@@ -0,0 +1,16 @@
+package in.co.nmsworks.training.week2.day3;
+
+public class ShapeRunner {
+    public static void main(String[] args) {
+        Shape sh=new Shape();
+        Square sq=new Square();
+        sq.area();
+        sq.perimeter();
+        Rectangle rt=new Rectangle();
+        rt.area();
+        rt.perimeter();
+        Circle cr=new Circle();
+        cr.area();
+        cr.perimeter();
+    }
+}
