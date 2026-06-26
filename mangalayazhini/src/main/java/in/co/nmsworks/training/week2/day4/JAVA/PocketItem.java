734cbad80319408572c68e098fcd8053de47645c
Mangala Yazhini <mangalayazhini@nmsworks.co.in>
2026-06-26 09:14:19 +0530

    PocketItem class added to day4 Java

---
+++ mangalayazhini/src/main/java/in/co/nmsworks/training/week2/day4/JAVA/PocketItem.java
@@ -0,0 +1,27 @@
+package in.co.nmsworks.training.week2.day4.JAVA;
+
+public class PocketItem {
+
+    private String color;
+    private Double price;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public void carry(){
+        System.out.println("Items is carried by the Pocket item.");
+    }
+}
