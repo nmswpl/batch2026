4978a044f5bb13942f877b8389f329347198d23d
PriyadharshiniS <spriyadharshini@nmsworks.co.in>
2026-06-28 12:21:07 +0530

    Week-2 day-5 Implemented pattern printing using nested for loops and added the main file

---
+++ spriya/src/main/java/in/co/nmsworks/week2/day5/MainPattern.java
@@ -0,0 +1,14 @@
+package in.co.nmsworks.week2.day5;
+
+public class MainPattern {
+    public static void main(String[] args) {
+
+        PatternPrint pp=new PatternPrint();
+
+        pp.rightTriangle();
+        pp.invertedRightTriangle();
+        pp.leftAlignedTriangle();
+        pp.invertedLeftAlignedTriangle();
+        pp.fullPyramid();
+}
+}
--- spriya/src/main/java/in/co/nmsworks/week2/day5/PatternPrint.java
+++ spriya/src/main/java/in/co/nmsworks/week2/day5/PatternPrint.java
@@ -8,6 +8,7 @@ public class PatternPrint {
 
         System.out.println("----Right Triangle----");
 
+
         for (int i = 1; i <= ROWS; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
