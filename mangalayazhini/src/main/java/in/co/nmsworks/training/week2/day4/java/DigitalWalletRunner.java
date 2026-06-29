7e09c2acd2de1d211ee3434eaec1982e60a16131
MangalaYazhini-NMS <mangalayazhini@nmsworks.co.in>
2026-06-29 10:04:51 +0530

    Refactor DigitalWalletRunner by cleaning up code
    Removed redundant code and adjusted method calls.

--- mangalayazhini/src/main/java/in/co/nmsworks/training/week2/day4/java/DigitalWalletRunner.java
+++ mangalayazhini/src/main/java/in/co/nmsworks/training/week2/day4/java/DigitalWalletRunner.java
@@ -10,20 +10,7 @@ public class DigitalWalletRunner {
         myWallet.setMyCoins(myEarnings);
 
         myWallet.getMyCoins();
-        myWallet.carry();package in.co.nmsworks.training.week2.day4.java;
-
-public class DigitalWalletRunner {
-    public static void main(String[] args) {
-        Coin myEarnings = new Coin();
-        myEarnings.setDenomination(1000);
-
-
-        DigitalWallet myWallet = new DigitalWallet();
-        myWallet.setMyCoins(myEarnings);
-
-        myWallet.getMyCoins();
         myWallet.carry();
-
         myWallet.makePayment(450);
         myWallet.makePayment(200);
         myWallet.makePayment(300);
@@ -33,12 +20,3 @@ public class DigitalWalletRunner {
     }
 }
 
-
-        myWallet.makePayment(450);
-        myWallet.makePayment(200);
-        myWallet.makePayment(300);
-        myWallet.makePayment(75);
-
-        myWallet.getMyCoins();
-    }
-}
