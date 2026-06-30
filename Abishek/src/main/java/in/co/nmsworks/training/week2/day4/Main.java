276be7ca10bdf2186418e5638c78b160ab72c6ca
Abishek Raj <abishekraj@nmsworks.co.in>
2026-06-30 09:35:20 +0530

    Main for School bag inheritance

---
+++ Abishek/src/main/java/in/co/nmsworks/training/week2/day4/Main.java
@@ -0,0 +1,14 @@
+package in.co.nmsworks.training.week2.day4;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        schoolExample();
+    }
+
+    private static void schoolExample() {
+        SchoolBag sb = new SchoolBag();
+        sb.open();
+        sb.study();
+    }
+}
