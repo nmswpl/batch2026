c721d6c1414fff0792425dac2b3fb3e0850952de
Mangala Yazhini <mangalayazhini@nmsworks.co.in>
2026-07-06 21:56:00 +0530

    Average salary of intern program is completed

---
+++ mangalayazhini/src/main/java/in/co/nmsworks/training/week4/day1/InternRunner.java
@@ -0,0 +1,17 @@
+package in.co.nmsworks.training.week4.day1;
+
+public class InternRunner {
+    public static void main(String[] args) {
+
+        String[] names = {"Priya", "Amizh", "Nithya", "Aambal", "Harini"};
+        Integer[] salary = {15000, 20000, 17000, 18000, 19000};
+
+        Company company1 = new Company();
+
+        for (int i = 0; i < 5; i++) {
+            company1.addInterns(new Intern(names[i], salary[i]));
+        }
+
+        company1.calculateAvgInternSalary(company1.getInternList());
+    }
+}
