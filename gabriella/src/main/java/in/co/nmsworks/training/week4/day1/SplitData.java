68c5e14fb46f7da43a111ba3f4e56b81eaa6c4da
Gabriella D <dgabriella@nmsworks.co.in>
2026-07-08 11:21:36 +0530

    Fixed issues in SplitData.java

--- gabriella/src/main/java/in/co/nmsworks/training/week4/day1/SplitData.java
+++ gabriella/src/main/java/in/co/nmsworks/training/week4/day1/SplitData.java
@@ -36,9 +36,9 @@ public class SplitData {
             String[] splitData = datum.split(",");
             //System.out.println(Arrays.toString(splitData));
             int year = Integer.parseInt(splitData[0]);
-            String industry_code = splitData[1].replace("\"", "");
-            String industry_name = splitData[2].replace("\"", "");
-            String rme_size = splitData[3].replace("\"", "");
+            String industryCode = splitData[1].replace("\"", "");
+            String industryName = splitData[2].replace("\"", "");
+            String rmeSize = splitData[3].replace("\"", "");
             String variable = splitData[4].replace("\"", "");
             String value = splitData[5];
             String unit = splitData[6];
@@ -47,17 +47,17 @@ public class SplitData {
                  PreparedStatement preparedStatement2 = con.prepareStatement("insert into amount_unit values(?,?,?,?,?,?)")){
                 if (Objects.equals(unit, "\"DOLLARS(millions)\"")){
                     preparedStatement2.setInt(1, year);
-                    preparedStatement2.setString(2, industry_code);
-                    preparedStatement2.setString(3, industry_name);
-                    preparedStatement2.setString(4, rme_size);
+                    preparedStatement2.setString(2, industryCode);
+                    preparedStatement2.setString(3, industryName);
+                    preparedStatement2.setString(4, rmeSize);
                     preparedStatement2.setString(5, variable);
                     preparedStatement2.setString(6, value);
                     preparedStatement2.executeUpdate();
                 } else {
                     preparedStatement1.setInt(1, year);
-                    preparedStatement1.setString(2, industry_code);
-                    preparedStatement1.setString(3, industry_name);
-                    preparedStatement1.setString(4, rme_size);
+                    preparedStatement1.setString(2, industryCode);
+                    preparedStatement1.setString(3, industryName);
+                    preparedStatement1.setString(4, rmeSize);
                     preparedStatement1.setString(5, variable);
                     preparedStatement1.setString(6, value);
                     preparedStatement1.executeUpdate();
@@ -75,9 +75,8 @@ public class SplitData {
 
     private List<String> readFromDB() {
         List<String> data = new ArrayList<>();
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("/home/nms-training/Downloads/annual-enterprise-survey-2025-financial-year-provisional-size-bands.csv"));
-             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/training", "nms-training", "");
-             Statement statement = con.createStatement()){
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("/home/nms-training/Downloads/annual-enterprise-survey-2025-financial-year-provisional-size-bands.csv"))
+             ){
             String line = "";
             bufferedReader.readLine();
             while ((line = bufferedReader.readLine()) != null){
