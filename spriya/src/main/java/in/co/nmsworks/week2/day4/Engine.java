dc9c6894aef1ea01bd61533dfad998f7fdb1f216
PriyadharshiniS <spriyadharshini@nmsworks.co.in>
2026-07-08 14:05:14 +0530

    Smart car system issue fixed

--- spriya/src/main/java/in/co/nmsworks/week2/day4/Engine.java
+++ spriya/src/main/java/in/co/nmsworks/week2/day4/Engine.java
@@ -1,10 +1,9 @@
 package in.co.nmsworks.week2.day4;
 
 public class Engine {
-    String horsepower;
 
 
-    public void startEngine(){
+    public void startEngine() {
         System.out.println("Start the Engine");
     }
 }
--- spriya/src/main/java/in/co/nmsworks/week2/day4/SmartCar.java
+++ spriya/src/main/java/in/co/nmsworks/week2/day4/SmartCar.java
@@ -1,9 +1,10 @@
 package in.co.nmsworks.week2.day4;
 
-public class SmartCar extends Vehicle{
-    int batteryLevel;
-  Engine engine;
-    public  void startCar(){
-      engine.startEngine();
+public class SmartCar extends Vehicle {
+
+    Engine engine;
+
+    public void startCar() {
+        engine.startEngine();
     }
 }
