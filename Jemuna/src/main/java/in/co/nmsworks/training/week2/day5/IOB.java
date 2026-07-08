605214d210308317d2854ddcbb400cd058ad398e
Jemuna <jemunau@nmsworks.co.in>
2026-07-08 15:08:13 +0530

    Week 2 day 5 removing reduntant data declaration in IOB Bank

--- Jemuna/src/main/java/in/co/nmsworks/training/week2/day5/IOB.java
+++ Jemuna/src/main/java/in/co/nmsworks/training/week2/day5/IOB.java
@@ -2,8 +2,8 @@ package in.co.nmsworks.training.week2.day5;
 
 public class IOB extends Bank{
     double rateOfInterest = 10;
+
     public double getRateOfInterest(){
-        double rateOfInterest = 10;
         return rateOfInterest;
     }
 }
