ca2def8f8481fdf044425076d91585e4ad90b521
Priyanka Kanagaraj <kpriyanka@nmsworks.co.in>
2026-07-08 14:43:50 +0530

    Unused comments are removed.

--- priyanka/src/main/java/in/co/nmsworks/training/week2/day3/TraineeRunner.java
+++ priyanka/src/main/java/in/co/nmsworks/training/week2/day3/TraineeRunner.java
@@ -5,7 +5,6 @@ import in.co.nmsworks.training.week2.day2.Trainee;
 public class TraineeRunner {
     public static void main(String[] args) {
         String[] names = {"arun","Balaji","Das","Gopi","Hari"};
-        //Integer[] ids = {1,2,3,4,5};
         int i=0;
         Trainee[] trainee = new Trainee[names.length];
         for (String name : names) {
--- priyanka/src/main/java/in/co/nmsworks/training/week2/day5/CreditCardPayment.java
+++ priyanka/src/main/java/in/co/nmsworks/training/week2/day5/CreditCardPayment.java
@@ -5,10 +5,4 @@ public class CreditCardPayment implements PaymentGateway{
     public void processPayment(double amount) {
         System.out.println("Processing credit card payment of " + amount);
     }
-
-
-//    @Override
-//    public void makePayment(PaymentGateway paymentGateway, double amount) {
-//
-//    }
 }
