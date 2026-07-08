c1d5b23f455ef5166585b4554c20eb7faa868235
PriyankaNMS <kpriyanka@nmsworks.co.in>
2026-07-08 10:13:53 +0530

    Change subject field to private in TextBook class

--- priyanka/src/main/java/in/co/nmsworks/training/week2/day4/java/TextBook.java
+++ priyanka/src/main/java/in/co/nmsworks/training/week2/day4/java/TextBook.java
@@ -1,7 +1,7 @@
 package in.co.nmsworks.training.week2.day4.java;
 
 public class TextBook {
-    public String subject = "Tamil";
+    private String subject = "Tamil";
 
     public void read(){
         System.out.println("Reading " + subject);
