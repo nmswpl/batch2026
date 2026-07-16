b852fe509da62a10e324dea16dc486984d88df18
MangalaYazhini-NMS <mangalayazhini@nmsworks.co.in>
2026-07-16 09:21:35 +0530

    Fix variable names and improve team score calculation

--- mangalayazhini/src/main/java/in/co/nmsworks/training/week3/day5/Team.java
+++ mangalayazhini/src/main/java/in/co/nmsworks/training/week3/day5/Team.java
@@ -4,28 +4,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Team {
-    List<Cricketer> teamMemebers = new ArrayList<>();
-    Integer teamScore = 0 ;
+    private List<Cricketer> teamMembers = new ArrayList<>();
+    private Integer teamScore = 0 ;
 
-    Team(List<Cricketer> teamMemebers){
-        this.teamMemebers = teamMemebers;
+    public Team(List<Cricketer> teamMembers){
+        this.teamMembers = teamMembers;
+        setTeamScore();
+
     }
 
     public void setTeamScore(){
-        for (Cricketer teamMemeber : teamMemebers) {
-            teamScore += teamMemeber.getTotalScore();
+        teamScore = 0;
+        for (Cricketer teamMember : teamMembers) {
+            teamScore += teamMember.getTotalScore();
         }
     }
 
     public Integer getTeamScore(){
-        setTeamScore();
-        System.out.println("The total score of " + teamMemebers.get(0).getTeam() + " : " + teamScore);
+        System.out.println("The total score of " + teamMembers.get(0).getTeam() + " : " + teamScore);
         return teamScore;
     }
 
     public void printTeamMembers(){
-        System.out.println("\n\n\nMemebers of " +teamMemebers.get(0).getTeam()+ " :");
-        for (Cricketer cricketer : teamMemebers) {
+        System.out.println("\n\n\nMembers of " +teamMembers.get(0).getTeam()+ " :");
+        for (Cricketer cricketer : teamMembers) {
             System.out.println(cricketer.getName() + " " + cricketer.getTeam() + " " + cricketer.getScorePoints() + " " + cricketer.getTotalScore());
         }
     }
